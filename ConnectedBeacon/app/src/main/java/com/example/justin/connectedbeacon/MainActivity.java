5473be58b088f2b3a1e8881e6221418b21574327
justin1093 <justinmnl@hotmail.com>
2017-05-22 19:41:35 +0530

    Connected Machines App Ver 3.0
    All screens are integrated

---
+++ ConnectedBeacon/app/src/main/java/com/example/justin/connectedbeacon/MainActivity.java
@@ -0,0 +1,53 @@
+package com.example.justin.connectedbeacon;
+
+import android.app.Activity;
+import android.app.ListActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.AdapterView.OnItemClickListener;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+import android.widget.Toast;
+
+
+public class MainActivity extends Activity{
+
+    ListView list;
+    String[] itemname ={
+            "Kiln",
+            "Pump",
+            "Conveyor",
+
+
+    };
+
+    Integer[] imgid={
+            R.drawable.pic1,
+            R.drawable.pic2,
+            R.drawable.pic3,
+
+
+    };
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        CustomListAdapter adapter=new CustomListAdapter(this, itemname, imgid);
+        list=(ListView)findViewById(R.id.list);
+        list.setAdapter(adapter);
+
+        list.setOnItemClickListener(new OnItemClickListener() {
+
+            @Override
+            public void onItemClick(AdapterView<?> parent, View view,
+                                    int position, long id) {
+                // TODO Auto-generated method stub
+                String Slecteditem= itemname[+position];
+                Toast.makeText(getApplicationContext(), Slecteditem, Toast.LENGTH_SHORT).show();
+
+            }
+        });
+    }
+}
